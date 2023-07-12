197c18d81a0a206990ebbe887b171eb1ba3ef38d
devkumar27 <dev270502@gmail.com>
2023-07-12 14:15:40 +0530

    finalising project

--- src/screens/NotFoundPage.js
+++ src/screens/NotFoundPage.js
@@ -6,11 +6,11 @@ const NotFoundPage = () => {
 		<div className="loading">
 			<h1>404 Page Not Found!</h1>
 			<div id="logo-name">
-				<b>Quiz</b>dom
+				<b>Quiz</b>-ADDA
 			</div>
 			<h3>
-				Who told you to mess up with the URL? <Link to="/">Click here</Link> and
-				Go back to the Dashboard.
+				<Link to="/">Click here</Link> to
+				go back to the Dashboard.
 			</h3>
 		</div>
 	)
